aeb665391805c51daee63aff2c61a333472a07ef
rakib38324 <rakib3832@gmail.com>
2022-08-22 16:46:43 +0600

    Commit the selest five player part

--- js/select.js
+++ js/select.js
@@ -1,16 +1,47 @@
-function Get_The_Access(element){
-    const getElementFild = document.getElementById(element);
-    return getElementFild;
+const nameArray = [];
+
+function display(Name){
+    console.log(Name)
+
+    const table = document.getElementById('playerName');
+
+    table.innerHTML = '';
+    for(let i=0; i< Name.length; i++){
+        
+        const Name= nameArray[i].PlayerName;
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <tr>
+            <th>${i+1}</th>
+            <th>${Name}</th>
+         </tr>
+        `;
+
+        if(i<5){
+            table.appendChild(tr);
+        }
+
+        else{
+            alert("You Cannot Select more than five.")
+        }
+
+        
+    }
 }
 
 
-document.getElementById('all-players-card-id').addEventListener('click',function(){
-    const selectPlayerFile = Get_The_Access('select-card');
-    const createArray = document.createElement('ul')
-
-    const playerTitle =document.getElementsByClassName('card-title')
 
 
-    createArray.innerText = playerTitle;
+function addToBox(element){
+    const PlayerName = element.parentNode.children[0].innerText;
+   
+    const nameObj ={
+        PlayerName: PlayerName
+    }
+    
+    nameArray.push(nameObj);
 
-})
+    display(nameArray);
+
+    
+}
